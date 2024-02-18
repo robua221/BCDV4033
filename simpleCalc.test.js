745eb3dd99970d503b1c317dddc05cf6ccf00fef
ROBIN <robinalexander20@gmail.com>
2024-02-17 21:10:14 -0500

    remove multiply

--- simpleCalc.test.js
+++ simpleCalc.test.js
@@ -17,14 +17,6 @@ describe("Calculator Tests", () => {
     expect(result).toBe(8);
   });
 
-  test("Multiplication of 2 numbers", () => {
-    // arrange and act
-    var result = mathOperations.product(2, 8);
-
-    // assert
-    expect(result).toBe(27);
-  });
-
   test("Division of 2 numbers", () => {
     // arrange and act
     var result = mathOperations.divide(24, 8);
